8a6f1902ba2ee881f1e282a0122c1cc9e91be3de
fagerbergj <fagerbergj1@wit.edu>
2018-08-02 09:23:03 -0400

    everything broken

--- sudoku_solver.c
+++ sudoku_solver.c
@@ -23,8 +23,8 @@ int validMove(int* puzzle, int x, int y, int num){
 
 	for(int i=0; i<n*n; ++i){
 		if (puzzle[dim*x + i] == num) return 0;
-		if (puzzle[dim*i + y] == num) return 0;
-		if (puzzle[dim*(rowStart + (i%n*n)) + (colStart + (i/n*n))] == num) return 0;
+		if (puzzle[dim*i + y] == num) return 0;//?
+		//if (puzzle[dim*(rowStart + (i%n*n)) + (colStart + (i/n*n))] == num) return 0;
 	}
 	return 1;
 }
@@ -40,18 +40,18 @@ int* genPuzzle(){
 	}
 
 	//n*n random spots
-	for(int i = 1; i <= n*n; i++){
-		int randPos = rand() % totalSize;
-		int x = getX(randPos);
-		int y = getY(randPos);
-
-		while(!validMove(puzzle, x, y, i)){
-			x = getX(randPos);
-                	y = getY(randPos);
-			randPos = rand() % totalSize;
-		}
-		puzzle[randPos] = i;
-	}
+//	for(int i = 1; i <= n*n; i++){
+//		int randPos = rand() % totalSize;
+//		int x = getX(randPos);
+//		int y = getY(randPos);
+//
+//		while(!validMove(puzzle, x, y, i)){
+//			x = getX(randPos);
+//               	y = getY(randPos);
+//			randPos = rand() % totalSize;
+//		}
+//		puzzle[randPos] = i;
+//	}
 	return puzzle;
 }
 
@@ -81,7 +81,7 @@ int main(int argc, char* argv){
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	//size of inner squares n*n
-	n = 3;
+	n =6;
 	//number of row/col in puzzle
 	dim = n*n;
 	//total number of elements in puzzle dim*dim
